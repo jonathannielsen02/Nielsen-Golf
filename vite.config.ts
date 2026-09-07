56449a8182ef4f173ba0128989fdc0b65414ae4d
jonathannielsen02 <jonathan.nielsen02@gmail.com>
2026-09-07 16:06:37 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-base: './',
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
@@ -6,6 +5,8 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: './',
+    
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
